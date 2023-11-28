2ac7d4964a35aad0dfa83e95656454f2577fe160
Arka Radithya <arkaradithya47@gmail.com>
2023-11-29 04:26:56 +0700

    nyoba add blog 1

---
+++ frontend/pages/blog/1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="app">
+      <Navbar />
+      <div class="content">
+        <div class="greetings">
+          <h3>
+            INI ISI BLOG 1, TAPI BELOM ADA ISINYA
+          </h3>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  </template>
