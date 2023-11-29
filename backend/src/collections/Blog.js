4b561993b832ba0ea4c942ea9c5e4ce1e0ea69b2
Arka Radithya <arkaradithya47@gmail.com>
2023-11-29 18:37:46 +0700

    backend blog, author, log

---
+++ backend/src/collections/Blog.js
@@ -0,0 +1,68 @@
+import payload from 'payload';
+
+/** @type {import('payload/types').CollectionConfig} */
+const Blog = {
+  slug: 'blog',
+  access: {
+    read: () => true,
+    update: () => true,
+    delete: () => true,
+    create: () => true,
+  },
+
+  hooks: {
+    afterOperation: [
+      async (args) => {
+        const collectionName = 'blog'; // Nama koleksi (collection) di mana aksi terjadi
+        const logAction = args.operation.charAt(0).toUpperCase() + args.operation.slice(1); // Capitalize operation
+
+        // Hanya buat log untuk operasi create, update, dan delete
+        if (['create', 'update', 'delete'].includes(args.operation)) {
+          console.log('Operation:', args.operation);
+
+          await payload.create({
+            collection: 'Logs',
+            data: {
+              name: args.result.title, // Ganti dengan field yang sesuai
+              log: args.result.id,
+              timestamp: new Date(),
+              action: `Blog ${logAction}`,
+              where: collectionName,
+            },
+          });
+        }
+      },
+    ],
+  },
+
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'date',
+      type: 'date',
+      admin: {
+        date: {
+          displayFormat: 'd MMM yyy',
+          pickerAppearance: 'dayOnly',
+        },
+      },
+    },
+    {
+      name: 'writer',
+      type: 'relationship',
+      required: true,
+      relationTo: 'author',
+    },
+    {
+      name: 'content',
+      type: 'textarea',
+      required: true,
+    },
+  ],
+};
+
+export default Blog;
--- backend/src/collections/Logs.js
+++ backend/src/collections/Logs.js
@@ -1,39 +1,40 @@
 /** @type {import('payload/types').CollectionConfig} */
 const Logs = {
-    slug: 'Logs', // Anda perlu menambahkan slug yang sesuai
-    admin : {
-      useAsTitle : 'name'
+  slug: 'Logs',
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {
+    read: () => true,
+    update: () => true,
+    delete: () => true,
+    create: () => false,
+  },
+  fields: [
+    {
+      name: 'name',
+      label: 'Title',
+      type: 'text',
     },
-    access: {
-      read: () => true,
-      update: () => true,
-      delete: () => true,
-      create: () => false,
+    {
+      name: 'log',
+      type: 'text',
     },
-  
-    fields: [
-      {
-        name: 'name',
-        label: 'Title',
-        type: 'text',
-      },
-      {
-        name: 'log',
-        type: 'text',
-      },
-      {
-        name: "timestamp",
-        type: "date",
-        required: true
-      },
-      {
-        name: "action",
-        type: "text",
-        required: true
-      }
-    ],
-    
-  };
-  
-  export default Logs;
-  
+    {
+      name: 'where',
+      type: 'text',
+    },
+    {
+      name: 'timestamp',
+      type: 'date',
+      required: true,
+    },
+    {
+      name: 'action',
+      type: 'text',
+      required: true,
+    },
+  ],
+};
+
+export default Logs;
